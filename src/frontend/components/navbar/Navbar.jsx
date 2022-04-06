1883bdb85e7d295ac8d1ee1248f3eb3943756610
prathmesh <prathmeshjagtap405@gmail.com>
2022-04-06 22:19:54 +0530

    added hamburger navigation
    added navigation for mobile view

--- src/frontend/components/navbar/Navbar.jsx
+++ src/frontend/components/navbar/Navbar.jsx
@@ -10,15 +10,66 @@ function Navbar() {
 	const { isAuth } = authState;
 	const { actionDispatch } = useActionContext();
 	const [searchInput, setSearchInput] = useState("");
+	const [navMobile, setNavMobile] = useState(false);
 
 	const navigate = useNavigate();
-
 	return (
 		<nav className="navigation__component">
 			<div className="nav">
-				<a href="/" className="toggles">
-					<i className="fas fa-bars"></i>
-				</a>
+				<p className="toggles" onClick={() => setNavMobile(!navMobile)}>
+					<i className={` ${navMobile ? "fas fa-times" : "fas fa-bars"}`}></i>
+				</p>
+				{navMobile ? (
+					<div className="mobile__navigation">
+						<p
+							className="navigation__link__mobile"
+							onClick={() => {
+								navigate("/");
+								setNavMobile(false);
+							}}
+						>
+							<i className="fas fa-home"></i>Home
+						</p>
+						<p
+							className="navigation__link__mobile"
+							onClick={() => {
+								navigate("/history");
+								setNavMobile(false);
+							}}
+						>
+							<i className="fas fa-history"></i>History
+						</p>
+
+						<p
+							className="navigation__link__mobile"
+							onClick={() => {
+								navigate("/liked");
+								setNavMobile(false);
+							}}
+						>
+							<i className="fas fa-heart"></i>Liked
+						</p>
+						<p
+							className="navigation__link__mobile"
+							onClick={() => {
+								navigate("/playlist");
+								setNavMobile(false);
+							}}
+						>
+							<i className="fas fa-play-circle"></i>PlayList
+						</p>
+						<p
+							className="navigation__link__mobile"
+							onClick={() => {
+								navigate("/watchLater");
+								setNavMobile(false);
+							}}
+						>
+							<i className="fas fa-clock"></i>Watch Later
+						</p>
+					</div>
+				) : null}
+
 				<Link className="nav__logo" to="/">
 					<p className="nav__logo-image">3am VIBES</p>
 				</Link>
